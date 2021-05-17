a0092753a1305a74d7939543b6bd493c920c450b
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-05-18 01:19:42 +0530

    Changed to CrudRepository

--- src/main/java/com/example/demo/repository/TutorialRepository.java
+++ src/main/java/com/example/demo/repository/TutorialRepository.java
@@ -1,12 +1,11 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Tutorial;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+public interface TutorialRepository extends CrudRepository<Tutorial, Long> {
     List<Tutorial> findByPublished(boolean published);
-
     List<Tutorial> findByTitleContaining(String title);
 }
